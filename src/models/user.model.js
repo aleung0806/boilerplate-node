f8a42fe473355734e05f2302a606ed601522eec4
aleung0806 <aleung0806@gmail.com>
2023-05-08 20:59:18 -0700

    mongoose toJSON transform working

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,38 +1,45 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt')
-const config = require('../config/config')
+const config = require('../config/config');
+const { redis } = require('../config/config');
 
-const userSchema = mongoose.Schema({
-  {
-    username: {
-      type: String, 
-      required: true,
-      trim: true,
+const userSchema = mongoose.Schema(
+    {
+      username: {
+        type: String, 
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true, 
+        trim: true,
+        lowercase: true,
+      },
+      password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8,
+      }
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true, 
-      trim: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 8,
+    {
+      timestamps: true,
+      'toJSON': {
+        transform: (doc, ret, options) => {
+          return {
+            id: ret._id,
+            username: ret.username,
+            email: ret.email
+          }
+        }
+      }
     }
-  },
-  {
-    toJSON: {
-      transform: 
-    }
-  },
-  {
-    timestamps: true
-  }
-}
+  
 )
+
+
 
 userSchema.statics.emailExists = async function (email) {
   const user = await this.findOne({email: email})
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,7 +3,7 @@ const ApiError = require('../utils/ApiError')
 const userService = require('./user.service')
 
 const verify = async (email, password) => {
-  const user = await userService.getByEmail(email)
+  const user = await userService.getByEmail(email).toJSON()
   if (user){
     if (await user.passwordMatches(password)){
       return user
